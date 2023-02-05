ca63f95e5cf5a403bd0d3ddad7fc2a63966fe0e6
Loulax <loulaxx91@gmail.com>
2023-02-05 19:13:27 +0100

    Rewrite rules to perform the firewall

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ It is easy to setting up rules for any system, you can open/close/forward ports,
 He is very powerfull!
 You can show it on his official website https://netfilter.org/
 
+
+# Routing iptables
+To enable forward on linux system, you should edit one parameter in 2 distincts files
+## For IPv4
+/etc/sysctl.conf: net.ipv4.ip_forward=1
+/lib/systemd/system/ipv4/ip_forward: 1
+
+## For IPv6
+/etc/sysctl.conf: net.ipv6.ip_forward=1
+/lib/systemd/system/ipv6/ip_forward: 1 
--- iptables.sh
+++ iptables.sh
@@ -11,33 +11,23 @@ iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
 
-#GLOBAL
-iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+# ALLOW WEB REQUEST FROM LAN
+iptables -A INPUT -p tcp -m multiport --sports 80,443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 
-#LOOPBACK
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
+# ALLOW WEB ACCESS FROM WAN
+iptables -A INPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --sports 80,443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
-#DROP SCAN XMAS/NULL
-iptables -A INPUT -m conntrack --ctstate INVALID -p tcp --tcp-flags FIN,URG,PSH FIN,URG,PSH -j DROP
-iptables -A INPUT -m conntrack --ctstate INVALID -p tcp --tcp-flags ALL ALL -j DROP
-iptables -A INPUT -m conntrack --ctstate INVALID -p tcp --tcp-flags ALL NONE -j DROP
-iptables -A INPUT -m conntrack --ctstate INVALID -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
- 
-# DROP BROADCAST PACKETS 
-iptables -A INPUT -m pkttype --pkt-type broadcast -j DROP
+# ALLOW SSH FROM WAN
+iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
-# ALLOW HTTP/S TO ANYWHERE
-iptables -A OUTPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW -j ACCEPT
+# ALLOW NAT MASQUERADE
+iptables -A POSTROUTING -t nat -o <interface> -j MASQUERADE
 
-# ALLOW SSH TO THE SERVER
-iptables -A INPUT -p tcp --dport 2021 -m conntrack --ctstate NEW -j ACCEPT
-
-# ALLOW DNS TRAFFIC TO ANYWHERE
-iptables -A OUTPUT -p udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
-
+# ALLOW NAT PREROUTING 2345
+iptables -A PREROUTING -t nat -p tcp --dport 2345 -d <public_ip> -j DNAT  --to-destination <lan_ip>:2345 
 
 #LOG
 iptables -A INPUT -m limit --limit 50/sec -j LOG --log-prefix "INPUT: " --log-level 4
