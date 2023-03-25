1e37d70139bbccfa98f3f546ee05cb0c78dd71b2
Loulax <gestion-loulax@proton.me>
2023-03-25 15:00:44 +0100

    Add a new chain for allowed policy and iptables binary relative path to bypass any potential privilege escalation

--- iptables.sh
+++ iptables.sh
@@ -1,39 +1,47 @@
 #!/bin/bash
 
 #CLEAR IPTABLES RULES
-iptables -F
-iptables -X
-iptables -t nat -F
-iptables -t nat -X
+/usr/sbin/iptables -F
+/usr/sbin/iptables -X
+/usr/sbin/iptables -t nat -F
+/usr/sbin/iptables -t nat -X
 
 #SET DEFAULT POLICY
-iptables -P INPUT DROP
-iptables -P OUTPUT DROP
-iptables -P FORWARD DROP
+/usr/sbin/iptables -P fw_in_allowed DROP
+/usr/sbin/iptables -P fw_out_allowed DROP
+/usr/sbin/iptables -P FORWARD DROP
+
+#SET CUSTOM CHAIN
+iptables -N fw_in_allowed
+iptablkes -N fw_out_allowed
+
+#REDIRECT DEFAULT CHAIN TO NEW
+iptables -A INPUT -j fw_in_allowed
+iptables -A OUPUT -j fw_out_allowed
 
 # ALLOW WEB REQUEST FROM LAN
-iptables -A INPUT -p tcp -m multiport --sports 80,443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+/usr/sbin/iptables -A fw_in_allowed -p tcp -m multiport --sports 80,443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+/usr/sbin/iptables -A fw_in_allowed -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 
 # ALLOW WEB ACCESS FROM WAN
-iptables -A INPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp -m multiport --sports 80,443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+/usr/sbin/iptables -A fw_in_allowed -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+/usr/sbin/iptables -A fw_out_allowed -p tcp -m multiport --sports 80,443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
 # ALLOW DNS REQUEST FROM LAN
-iptables -A INPUT -p udp --dport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p udp --sport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+/usr/sbin/iptables -A fw_in_allowed -p udp --dport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+/usr/sbin/iptables -A fw_out_allowed -p udp --sport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 
 # ALLOW SSH FROM WAN
-iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+/usr/sbin/iptables -A fw_in_allowed -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+/usr/sbin/iptables -A fw_out_allowed -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
 # ALLOW NAT MASQUERADE
-iptables -A POSTROUTING -t nat -o <interface> -j MASQUERADE
+/usr/sbin/iptables -A POSTROUTING -t nat -o <interface> -j MASQUERADE
 
 # ALLOW NAT PREROUTING 2345
-iptables -A PREROUTING -t nat -p tcp --dport 2345 -d <public_ip> -j DNAT  --to-destination <lan_ip>:2345 
+/usr/sbin/iptables -A PREROUTING -t nat -p tcp --dport 2345 -d <public_ip> -j DNAT  --to-destination <lan_ip>:2345 
 
 #LOG
-iptables -A INPUT -m limit --limit 50/sec -j LOG --log-prefix "INPUT: " --log-level 4
-iptables -A OUTPUT -m limit --limit 50/sec -j LOG --log-prefix "OUTPUT: " --log-level 4
-iptables -A FORWARD -m limit --limit 50/sec -j LOG --log-prefix "FORWARD: " --log-level 4
+/usr/sbin/iptables -A INPUT -m limit --limit 50/sec -j LOG --log-prefix "INPUT DROP: " --log-level 4
+/usr/sbin/iptables -A OUTPUT -m limit --limit 50/sec -j LOG --log-prefix "OUTPUT DROP: " --log-level 4
+/usr/sbin/iptables -A FORWARD -m limit --limit 50/sec -j LOG --log-prefix "FORWARD DROP: " --log-level 4
